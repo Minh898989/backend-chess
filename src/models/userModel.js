01739eb823403b705bb08a7b4a54b31be11c1966
Minh898989 <minhhh270805@gmail.com>
2025-05-09 23:34:11 +0700

    gh

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,14 @@
 const db = require('../config/db');
 
 const findUserByUserId = (userid, callback) => {
-  db.query('SELECT * FROM users WHERE userid = ?', [userid], callback);
+  db.query('SELECT * FROM users WHERE userid = $1', [userid], (err, result) => {
+    if (err) return callback(err);
+    callback(null, result.rows);
+  });
 };
 
 const createUser = (userid, password, callback) => {
-  db.query('INSERT INTO users (userid, password) VALUES (?, ?)', [userid, password], callback);
+  db.query('INSERT INTO users (userid, password) VALUES ($1, $2)', [userid, password], callback);
 };
 
 module.exports = { findUserByUserId, createUser };
