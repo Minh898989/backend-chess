04e1967f11b7372f8fc5be91764710ab0d0ba5bd
Minh898989 <minhhh270805@gmail.com>
2025-05-09 13:04:35 +0700

    lk

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: '27082005', // your password
   database: 'chess',
-  port: 3306
+  
 });
 
 connection.connect((err) => {
