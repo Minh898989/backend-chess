472567ed16c30feff01e40ca33a8a0da47541e07
Minh898989 <minhhh270805@gmail.com>
2025-06-10 10:08:27 +0700

    a

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,24 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'dpg-d0f2lr49c44c738kr7ug-a.oregon-postgres.render.com',
-  user: 'sql_xi3o_user',
-  password: 'sYrD5vTPSuZeJaJaiR6Gm1X4BbtBLjPR',
-  database: 'sql_xi3o',
+  host: 'dpg-d13plmogjchc73feck30-a.oregon-postgres.render.com',
+  user: 'sql_calc_user',
+  password: 'gF6H8xoURWaMTwEVnkCzhzajrZLKNaoZ',
+  database: 'sql_calc',
   port: 5432,
   ssl: {
-    rejectUnauthorized: false
+    require: true,               
+    rejectUnauthorized: false   
   }
 });
 
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack);
-  }
-  console.log('Connected to PostgreSQL');
-  release();
-});
+pool.connect()
+  .then(client => {
+    console.log('✅ Connected to PostgreSQL');
+    client.release();
+  })
+  .catch(err => {
+    console.error('❌ Connection error', err.stack);
+  });
 
 module.exports = pool;
